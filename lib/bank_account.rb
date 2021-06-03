57595b563dff239308c796ad027a863dae12f987
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-06-03 16:42:22 +0100

    Created seperate printer class for printing statements

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -26,14 +26,11 @@ class BankAccount
     @balance -= amount
     create_transaction(0, amount, @balance)
   end
-
+  
   def print_statement
-    puts 'date || credit || debit || balance'
-    @transactions.reverse.map do |transaction|
-      puts "#{transaction.date} || #{'%.2f' % transaction.credit} || #{'%.2f' % transaction.debit} || #{'%.2f' % transaction.current_balance}"
-    end
+    @bank_statement.print(@transactions)
   end
-
+  
   def create_transaction(amount, balance, date)
     transaction = @transaction.new(amount, balance, date)
     @transactions << transaction
---
+++ lib/printer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Printer
+  def print(transactions)
+    @transactions = transactions
+    puts print_statement
+  end
+
+  def print_statement
+    puts 'date || credit || debit || balance'
+    @transactions.reverse.map do |transaction|
+      puts "#{transaction.date} || #{'%.2f' % transaction.credit} || #{'%.2f' % transaction.debit} || #{'%.2f' % transaction.current_balance}"
+    end
+  end
+  
+end
